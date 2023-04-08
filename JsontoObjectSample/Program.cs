aeedf1a4d823ea262bcd3f116b5eb15f9cb323be
aksha <aksha@DESKTOP-2M0CCPM>
2023-04-08 11:25:02 +0530

    Final Commit : Convert json data to c# object

--- JsontoObjectSample/Program.cs
+++ JsontoObjectSample/Program.cs
@@ -1,7 +1,44 @@
-﻿internal class Program
+﻿using JsontoObjectSample;
+using Newtonsoft.Json;
+using RestSharp;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        GetData();
+    }
+    public static void GetData()
+    {
+        try
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            var client = new RestClient(url);
+            var request = new RestRequest();
+            var response = client.Get(request);
+            if (response.IsSuccessStatusCode)
+            {
+                string? rawData = response.Content;
+                List<JsonPosts>? jsonposts = JsonConvert.DeserializeObject<List<JsonPosts>>(rawData);
+                foreach (JsonPosts item in jsonposts)
+                {
+                    int id = item.Id;
+                    string? body = item.Body;
+                    string? title = item.Title;
+                    Console.WriteLine("Id : " + id);
+                    Console.WriteLine("Title : " + title);
+                    Console.WriteLine("Body : " + body);
+                    Console.WriteLine("----------------------------------------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: Not found");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
     }
 }
